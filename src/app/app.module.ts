e67fbc57ddb2518d9fcb2482053f68c92c87c1f5
aymen <aymen2255@hotmail.fr>
2024-05-12 22:30:43 +0200

    import BrowserAnimationsModule necessary to animate line charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { DetailComponent } from './pages/detail/detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent, NavBarComponent, DetailComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule,NgxChartsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule,NgxChartsModule,BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
