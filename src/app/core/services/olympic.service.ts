242bb6d246614b229a2429159a743ca2d3be76e6
aymen <aymen2255@hotmail.fr>
2024-05-12 20:10:50 +0200

    improve error handling for loadInitialData method in OlympicService

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root',
@@ -13,14 +15,17 @@ export class OlympicService {
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
-        return caught;
+        this.olympics$.next([]);
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: `Error loading initial data`
+        });
+        return throwError(() => new Error(`Error loading initial data`));
       })
     );
   }
